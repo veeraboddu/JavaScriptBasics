eb057a83540fc4faa0910c88fc61a54148e2c429
Veera Boddu <30390457+veeraboddu@users.noreply.github.com>
2020-03-29 21:44:18 -0700

    Create webDriverIO.js
    webdriverIO basic concepts and install vsc node npm mocha chai

---
+++ webDriverIO.js
@@ -0,0 +1,21 @@
+// webDriverIO is a openscource framework for the javascript automation. Custom implementation for selenium webdriver API.  
+// webDriverIO is a common framework tool for - Cucumber , Mocha and Jasmine. 
+It is written in javascript and packaged into npm and runs on node js server
+Automate both Web applicaiton and native mobile apps
+// allure reporting, Jasmine spec reporter, junit xml reporting, Cucumber reporting
+Android , IOS
+synchronics exections.
+page oject model
+continuous integration jenkins
+retrying if any failure to execute the testcases again
+installation:
+1.java, 2. node js, 3. npm installation (node -v, npm -v), 4. install Visual Studio code
+open vsc - config the work space, go to terminal - npm init - package.json will be configured with name, version, main, desc,script,author,
+license
+commands
+npm init
+npm install webdriverio --save-dev   // webdriveio version will be added to the package.json
+npm install @wdio/cli  
+./node_modules/.bin/wdio config  //WDIO configuration helper // configuring config.js file will be added// wdio.config.js will be added
+npm install chai --save-dev
+npm install chai-webdriverio --save-dev
