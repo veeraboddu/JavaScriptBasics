d9310bf0d61c35a9aa0b9500b500cc4452aa94cb
Veera Boddu <30390457+veeraboddu@users.noreply.github.com>
2020-04-01 21:17:58 -0700

    Update JSBasicCocepts.js
    interview js QA tricky

--- JSBasicCocepts.js
+++ JSBasicCocepts.js
@@ -335,3 +335,103 @@ const theromst = new FirstClass(98);
 
 //import {capitalizeString } from "./string_function"
 // another js file path
+// js interview qa
+var global = "My global variable"
+
+function personalDetails() {
+   let name = 'veera'
+    if (name == 'veera'){
+        let desig = "QA Automation"
+        console.log(desig)
+    }
+    console.log(name)
+    console.log(global)
+  //  console.log(desig)
+
+}
+personalDetails()
+console.log(global)
+
+// How to create a private variable in JS
+
+function secret() {
+    var sample = "secret value"
+    return function() {
+        return  sample
+    }
+}
+var test = secret()
+console.log(secret())
+//console.log(test)
+
+console.log(typeof 1)   // typeof 1 gives you number typeof typeof 1  return string
+
+//---tricky question
+console.log(2+'2')  // answer 22
+console.log(2-'2') // answer 0
+
+let arrVal = [1,2,2,3]
+console.log(new Set(arrVal))   // remove the duplicate elements
+console.log([...new Set(arrVal)])// convert the Set object into array
+
+let func = function () {
+
+    {
+        let l = 'let'
+        var v = 'var'
+    }
+    console.log(v)      // var is global
+   // console.log(l)   // let with in the block
+}
+func()
+// next question
+console.log(5 < 6 <7)  // true  5 < 6  true < 7 ie 1 < 7 = true
+console.log(7>6>5) //false 7>6 true>5 ie 1>5 = false
+// next
+let x = function () {
+    return    {   // it return the value  if we keep { in the next line we will get undefined.
+        message:'hi'
+    }
+}
+console.log(x())
+
+// how to add a property to the object
+let profile = {
+    name : 'veera'
+};
+profile.age = 30   // we can add the property to the object
+console.log(profile)
+profile.desig = 'Software'
+Object.freeze(profile)  // not allow them to add new property but now modify the property
+//Object.seal(profile)   // seal will not  allow to modify or add new property
+profile.lastname = 'boddu'
+console.log(profile)
+
+// how to modify only ony property
+
+let profile1 = {
+    name:'meghana',
+};
+
+Object.defineProperty(profile1,'age',{
+    value:3,
+    writale:false
+})
+
+profile1.name = 'newValue' // it allow to modify
+//profile1.age = 5; // it will not allow to modify writable
+
+// largest numbe
+console.log(Math.max())  // -infinity because it will take least value compart the given value
+console.log(Math.max(123,9,100,101))  // max is123
+
+console.log(Math.min(123,9,100,101))  // max is123
+console.log(Math.floor(Math.random()*5))
+// iterate the json object in js  sinle
+var myJsonobj = {id:112345,name:'veera',desig:'software',location:'sanbruno',state:'CA'}
+console.log(myJsonobj.id);
+console.log((myJsonobj['name']));
+console.log((myJsonobj['desig']));
+
+console.log('-----------------------');
+
